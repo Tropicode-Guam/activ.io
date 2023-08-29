74b86c9837ab261d94576ebc29f71c4b7d6bc544
chovin <chovin@users.noreply.github.com>
2023-08-29 22:14:22 +1000

    example serverside generated places from an api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
-export default function Home() {
+const API_BASE = process.env.API_BASE
+
+export default async function Home() {
+  const places = await fetch(API_BASE + '/places').then((res) => res.json())
+
   return (
     <div>
       <h1>Hello World</h1>
       <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      <ul>
+        {
+          places.map((place: { id: number; title: string }) => (
+            <li key={place.id}>
+              <Link href={"/places/" + place.id}>
+                {place.title}
+              </Link>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
