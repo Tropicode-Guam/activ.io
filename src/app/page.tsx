49f5cef756faf490786896925861abee57be0dbf
leocodesthings <106573277+leocodesthings@users.noreply.github.com>
2023-09-07 00:10:16 +1000

    Add experimental location card layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
-import Map from './components/Map'
 import { Point } from './components/Map'
+import Map from './components/Map'
+import Image from "next/image";
 
 const API_BASE = process.env.API_BASE
 
@@ -8,15 +9,25 @@ export default async function Home() {
   const places: Point[] = await fetch(API_BASE + '/places').then((res) => res.json())
 
   return (
-    <div>
+    <div className="mx-auto">
       <Map points={places}/>
-      <ul>
+      <ul className="grid justify-items-center gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {
-          places.map((place: { id: number; title: string }) => (
+          places.map((place: Point ) => (
             <li key={place.id}>
-              <Link href={"/places/" + place.id}>
-                {place.title}
-              </Link>
+                <Link href={"/places/" + place.id}>
+                    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                        <Image className="w-full" src={place.img} alt={place.title} width={400} height={100}/>
+                        <div className="px-6 py-4">
+                            <div className="font-bold text-xl mb-2">{place.title}</div>
+                            <p className="text-gray-700 text-base card-desc" >{place.desc}
+                            </p>
+                        </div>
+                        <div className="px-6 pt-4 pb-2">
+                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{place.type}</span>
+                        </div>
+                    </div>
+                </Link>
             </li>
           ))
         }
